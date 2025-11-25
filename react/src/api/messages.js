a1fe7eb805ff98c8bc9312cb815463038444e5cc
IbragimKhaniev <newinterny@gmail.com>
2025-11-25 14:19:46 +0300

    Add messages API with getMessages and sendMessage functions

--- react/src/api/messages.js
+++ react/src/api/messages.js
@@ -1,35 +1,39 @@
-import instance from './axios';
-import { getToken } from '../utils/auth';
+import instance from './axios.js';
+import { getToken } from '../utils/auth.js';
 
 /**
  * Get all chat messages
- * @returns {Promise} - Returns array of messages
+ * @returns {Promise} - Promise with messages array
  */
 export const getMessages = async () => {
   const token = getToken();
+  
   const response = await instance.get('/api/messages/', {
     headers: {
-      Authorization: `Token ${token}`,
+      'Authorization': `Bearer ${token}`,
     },
   });
+  
   return response.data;
 };
 
 /**
- * Send a new chat message
+ * Send a new message to chat
  * @param {string} text - Message text content
- * @returns {Promise} - Returns created message data
+ * @returns {Promise} - Promise with created message object
  */
 export const sendMessage = async (text) => {
   const token = getToken();
+  
   const response = await instance.post(
     '/api/messages/',
     { text },
     {
       headers: {
-        Authorization: `Token ${token}`,
+        'Authorization': `Bearer ${token}`,
       },
     }
   );
+  
   return response.data;
 };
